a6163a7abb650b3316cee8bd278cd13bd0f8aa98
xendv <dvoynina.ksusha@mail.ru>
2021-04-10 10:58:41 +0300

    added Toolbar with icons and PictureBox

--- OOAtask7.4/MyForm.h
+++ OOAtask7.4/MyForm.h
@@ -46,6 +46,11 @@ namespace OOAtask74 {
 	private: System::Windows::Forms::ToolStripMenuItem^ ‚‘ÓÏ‡ÚÂJPGToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^ ‚‘ÓÏ‡ÚÂPNGToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^ ‚˚ıÓ‰ToolStripMenuItem;
+	private: System::Windows::Forms::ToolStrip^ toolStrip;
+	private: System::Windows::Forms::ToolStripButton^ toolStripButton1;
+	private: System::Windows::Forms::ToolStripButton^ toolStripButton2;
+	private: System::Windows::Forms::ToolStripButton^ toolStripButton3;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
 	protected:
 
 	private:
@@ -61,6 +66,7 @@ namespace OOAtask74 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->menuStrip = (gcnew System::Windows::Forms::MenuStrip());
 			this->Ù‡ÈÎToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->‚˚ıÓ‰ToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
@@ -68,7 +74,14 @@ namespace OOAtask74 {
 			this->Á‡„ÛÁËÚ¸»ÁÓ·‡ÊÂÌËÂToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->‚‘ÓÏ‡ÚÂJPGToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->‚‘ÓÏ‡ÚÂPNGToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->toolStrip = (gcnew System::Windows::Forms::ToolStrip());
+			this->toolStripButton1 = (gcnew System::Windows::Forms::ToolStripButton());
+			this->toolStripButton2 = (gcnew System::Windows::Forms::ToolStripButton());
+			this->toolStripButton3 = (gcnew System::Windows::Forms::ToolStripButton());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			this->menuStrip->SuspendLayout();
+			this->toolStrip->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// menuStrip
@@ -79,7 +92,7 @@ namespace OOAtask74 {
 			});
 			this->menuStrip->Location = System::Drawing::Point(0, 0);
 			this->menuStrip->Name = L"menuStrip";
-			this->menuStrip->Size = System::Drawing::Size(284, 24);
+			this->menuStrip->Size = System::Drawing::Size(364, 24);
 			this->menuStrip->TabIndex = 0;
 			this->menuStrip->Text = L"menuStrip";
 			// 
@@ -127,17 +140,79 @@ namespace OOAtask74 {
 			this->‚‘ÓÏ‡ÚÂPNGToolStripMenuItem->Size = System::Drawing::Size(160, 22);
 			this->‚‘ÓÏ‡ÚÂPNGToolStripMenuItem->Text = L"¬ ÙÓÏ‡ÚÂ PNG";
 			// 
+			// toolStrip
+			// 
+			this->toolStrip->AutoSize = false;
+			this->toolStrip->Dock = System::Windows::Forms::DockStyle::Left;
+			this->toolStrip->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
+				this->toolStripButton1,
+					this->toolStripButton2, this->toolStripButton3
+			});
+			this->toolStrip->Location = System::Drawing::Point(0, 24);
+			this->toolStrip->Name = L"toolStrip";
+			this->toolStrip->Size = System::Drawing::Size(44, 317);
+			this->toolStrip->TabIndex = 1;
+			this->toolStrip->Text = L"toolStrip";
+			// 
+			// toolStripButton1
+			// 
+			this->toolStripButton1->AutoSize = false;
+			this->toolStripButton1->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Image;
+			this->toolStripButton1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"toolStripButton1.Image")));
+			this->toolStripButton1->ImageScaling = System::Windows::Forms::ToolStripItemImageScaling::None;
+			this->toolStripButton1->ImageTransparentColor = System::Drawing::Color::Magenta;
+			this->toolStripButton1->Name = L"toolStripButton1";
+			this->toolStripButton1->Size = System::Drawing::Size(42, 42);
+			this->toolStripButton1->Text = L"toolStripButton1";
+			// 
+			// toolStripButton2
+			// 
+			this->toolStripButton2->AutoSize = false;
+			this->toolStripButton2->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Image;
+			this->toolStripButton2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"toolStripButton2.Image")));
+			this->toolStripButton2->ImageScaling = System::Windows::Forms::ToolStripItemImageScaling::None;
+			this->toolStripButton2->ImageTransparentColor = System::Drawing::Color::Magenta;
+			this->toolStripButton2->Name = L"toolStripButton2";
+			this->toolStripButton2->Size = System::Drawing::Size(42, 42);
+			this->toolStripButton2->Text = L"toolStripButton2";
+			// 
+			// toolStripButton3
+			// 
+			this->toolStripButton3->AutoSize = false;
+			this->toolStripButton3->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Image;
+			this->toolStripButton3->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"toolStripButton3.Image")));
+			this->toolStripButton3->ImageScaling = System::Windows::Forms::ToolStripItemImageScaling::None;
+			this->toolStripButton3->ImageTransparentColor = System::Drawing::Color::Magenta;
+			this->toolStripButton3->Name = L"toolStripButton3";
+			this->toolStripButton3->Size = System::Drawing::Size(42, 42);
+			this->toolStripButton3->Text = L"toolStripButton3";
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Location = System::Drawing::Point(47, 27);
+			this->pictureBox1->MaximumSize = System::Drawing::Size(300, 300);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(300, 300);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 2;
+			this->pictureBox1->TabStop = false;
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(284, 261);
+			this->ClientSize = System::Drawing::Size(364, 341);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->toolStrip);
 			this->Controls->Add(this->menuStrip);
 			this->MainMenuStrip = this->menuStrip;
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->menuStrip->ResumeLayout(false);
 			this->menuStrip->PerformLayout();
+			this->toolStrip->ResumeLayout(false);
+			this->toolStrip->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
